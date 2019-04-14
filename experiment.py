661f2ac4b347a4f8c88ae5f4b2c27186ba65d3ef
Steven Chen <sccchenster@gmail.com>
2019-04-14 11:43:42 -0700

    Set idx range for experiment

--- experiment.py
+++ experiment.py
@@ -70,6 +70,7 @@ def main():
     parser.add_argument('--est-strat', type=str, default="uniform")
     parser.add_argument('--img-idxs', type=str, default=None, help='Image indices to use')
     parser.add_argument('--no-detection', action='store_true')
+    parser.add_argument('--idx-range', type=int, nargs=2)
     args = parser.parse_args()
 
     # Data checks
@@ -118,6 +119,17 @@ def main():
             img_idxs.append((orig_i, target_i, target_class))
         assert args.num_exp_per_param <= len(img_idxs)
 
+    timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M")
+    os.makedirs("./experiment_results", exist_ok=True)
+
+    results_npz = "./experiment_results/%s_%s" % (args.exp_name, timestamp)
+
+    idx_range_specified = args.idx_range is not None
+    if idx_range_specified:
+        start_idx, end_idx = args.idx_range[0], args.idx_range[1]
+        results_npz = "./experiment_results/%s_s%d_e%d_%s" % (args.exp_name, start_idx, end_idx, timestamp)
+
+
     for val in args.exp_param_range:
         set_param(args, args.exp_param, val)
         for val_2 in args.exp_param_range_2:
@@ -134,7 +146,8 @@ def main():
             print("[experiment] %s=%f, %s=%f" % (args.exp_param, val, args.exp_param_2, val_2))
             key = (val, val_2)
 
-            for i in range(args.num_exp_per_param):
+            idx_range = range(start_idx, end_idx) if idx_range_specified else range(args.num_exp_per_param)
+            for i in idx_range:
                 # if i % s == 0:
 
                 if imgs_specified:
@@ -204,9 +217,8 @@ def main():
     print("Summary:")
     for s in results_s:
         print(s)
-    timestamp = datetime.datetime.strftime(datetime.datetime.now(), "%Y%m%d_%H%M")
-    os.makedirs("./experiment_results", exist_ok=True)
-    np.savez_compressed("./experiment_results/%s_%s" % (args.exp_name, timestamp), attack_results=all_results,
+
+    np.savez_compressed(results_npz, attack_results=all_results,
                         detection_results=detection_results, main_results=main_results,
                         params=np.array(args.exp_param_range), args=vars(args))
 
